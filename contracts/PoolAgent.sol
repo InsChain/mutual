7bac08f9e5f34e2d5973bd4b43c42965eb4e785f
Stella <stellar@insChains-MacBook-Pro.local>
2018-05-02 14:28:47 +0800

    basic protocol for policy id recover from signature

--- contracts/PoolAgent.sol
+++ contracts/PoolAgent.sol
@@ -34,8 +34,25 @@ contract PoolAgent{
     }
     
     
-    function testAgent(uint256 payload, uint256 timestamp) onlyOwner public returns (bool success){
-        require(pool.joinWithCandy(msg.sender, payload, timestamp));
+    mapping(bytes => bool) usedNonces;
+    PolicyPoolInterface pool;
+    
+    function PoolAgent(address poolAddr) public {
+        pool=PolicyPoolInterface(poolAddr);
+        owner = msg.sender;
+    }
+
+
+
+    function claimPolicy(uint256 payload, bytes nonce, bytes sig) public returns (bool){
+        require(!usedNonces[nonce]);
+        usedNonces[nonce] = true;
+
+        // This recreates the message that was signed on the client.
+        bytes32 message = prefixed(nonce);
+
+        require(recoverSigner(message, sig) == msg.sender);
+        require(pool.joinWithCandy(msg.sender, payload, now));
         
         return true;
     }
@@ -45,4 +62,60 @@ contract PoolAgent{
         require(msg.sender == owner);
         selfdestruct(msg.sender);
     }
+
+
+    // Signature methods
+
+    function splitSignature(bytes sig) internal pure returns (uint8, bytes32, bytes32) {
+        require(sig.length == 65);
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        assembly {
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        return (v, r, s);
+    }
+
+    function recoverSigner(bytes32 message, bytes sig) internal pure returns (address){
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        (v, r, s) = splitSignature(sig);
+
+        return ecrecover(message, v, r, s);
+    }
+
+    // Builds a prefixed hash to mimic the behavior of eth_sign.
+    function prefixed(bytes hash) internal pure returns (bytes32) {
+        return keccak256("\x19Ethereum Signed Message:\n",bytes1(uintToBytes(hash.length)), hash);
+    }
+    
+    /// title String Utils - String utility functions
+    /// author Piper Merriam - <pipermerriam at gmail.com>
+    /// dev Converts an unsigned integert to its string representation.
+    /// param v The number to be converted.
+    function uintToBytes(uint v) internal pure returns (bytes32 ret) {
+        if (v == 0) {
+            ret = '0';
+        }
+        else {
+            while (v > 0) {
+                ret = bytes32(uint(ret) / (2 ** 8));
+                ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
+                v /= 10;
+            }
+        }
+        return ret;
+    }
+
 }
